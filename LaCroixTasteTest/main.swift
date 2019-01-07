4a7f6a5bc053cc4ed130daf41b5b3e50daf495da
Cara <cararbrennan@gmail.com>
2019-01-06 17:59:53 -0800

    Initial Commit

---
+++ LaCroixTasteTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LaCroixTasteTest
+//
+//  Created by Cara  Brennan on 1/6/19.
+//  Copyright © 2019 Cara  Brennan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
